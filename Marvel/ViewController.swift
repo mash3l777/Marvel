79b5ac57684e10d101c0b38521d8daa4c56af464
mash'al AL-Ma'mari <mash3l777@gmail.com>
2022-08-10 19:37:30 +0400

    Initial Commit

---
+++ Marvel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel
+//
+//  Created by مشعل المعمري on 10/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
